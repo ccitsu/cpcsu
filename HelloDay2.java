c6f71b212a26aefb143eb1c2cbcb946c2a365b91
nayyarahmed <ccitlms@gmail.com>
2022-06-29 14:36:34 +0300

    PCCommit

--- HelloDay2.java
+++ HelloDay2.java
@@ -1,5 +1,7 @@
 public class HelloDay2{
     public static void main(String args[]) {
         System.out.println("Hello Day 2 - CPC! Prep.");       
+        System.out.println("Let us learn Some advance JAVA Classes");       
+
     }
 }
